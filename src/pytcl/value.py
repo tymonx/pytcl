73e4b085a853b39218387bad49b738beec05e167
Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
2025-03-10 20:18:28 +0100

    feat: add more tests for TCL dict

--- src/pytcl/value.py
+++ src/pytcl/value.py
@@ -3,18 +3,18 @@
 
 """TCL value abstraction type for TCL string, list, dict, integer, boolean, floating point."""
 
-from typing import Tuple
+from typing import Any, Tuple
 from collections.abc import Iterable, Iterator
 
 
 class TCLValue:
-    def __init__(self, value: str):
+    def __init__(self, value: Any):
         """Create new instalce of TCL value from provided string that can be anything.
 
         Args:
             value: Any TCL value. Anything in TCL is a string.
         """
-        self._value: str = value
+        self._value: str = str(value)
 
     def __str__(self) -> str:
         return self._value
@@ -33,7 +33,11 @@ class TCLValue:
 
     def __len__(self) -> int:
         """Number of elements in TCL list."""
-        return len(list(self.__iter__()))
+        return len(self._value.split())
+
+    def __contains__(self, item: Any) -> bool:
+        """Check if provided item is part of TCL value."""
+        return str(item) in self._value
 
     def __getitem__(self, key: str | int) -> "TCLValue":
         """Get single TCL value from TCL list or dictionary using provided index or dictionary key."""
@@ -45,3 +49,11 @@ class TCLValue:
     def items(self) -> Iterable[Tuple[str, "TCLValue"]]:
         items: list[str] = self._value.split()
         return zip(items[0::2], (TCLValue(item) for item in items[1::2]))
+
+    def keys(self) -> list[str]:
+        """List of dictionary keys."""
+        return self._value.split()[0::2]
+
+    def values(self) -> list["TCLValue"]:
+        """List of dictionary values."""
+        return [TCLValue(item) for item in self._value.split()[1::2]]
--- tests/test_tclsh.py
+++ tests/test_tclsh.py
@@ -41,3 +41,29 @@ def test_tclsh_list_lappend() -> None:
 
         assert len(items) == 2
         assert str(items[1]) == "foo"
+
+
+def test_tclsh_dict_create() -> None:
+    """Test TCL shell: `set items [dict create]`"""
+    with PyTCL() as tclsh:
+        # TCL: set items [dict create a 1 b 2]
+        items = tclsh.set("items", tclsh.dict("create", "a", 1, "b", 2))
+
+        assert len(dict(items)) == 2
+        assert items.keys() == ["a", "b"]
+        assert [int(value) for value in items.values()] == [1, 2]
+        assert int(items["a"]) == 1
+        assert int(items["b"]) == 2
+
+
+def test_tclsh_dict_from_python_dict() -> None:
+    """Test TCL shell: `set items {}`"""
+    with PyTCL() as tclsh:
+        # TCL: set items [dict create a 1 b 2]
+        items = tclsh.set("items", {"c": 3, "d": 4})
+
+        assert len(dict(items)) == 2
+        assert items.keys() == ["c", "d"]
+        assert [int(value) for value in items.values()] == [3, 4]
+        assert int(items["c"]) == 3
+        assert int(items["d"]) == 4
