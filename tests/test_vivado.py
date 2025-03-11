7f1005cb9534782452f424c7986fb68112764f27
Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
2025-03-11 06:57:40 +0100

    feat: add test for launching synthesis and implementation runs for Xilinx Vivado

--- tests/test_vivado.py
+++ tests/test_vivado.py
@@ -8,11 +8,12 @@ from pathlib import Path
 from pytcl import Vivado, PyTCL
 import pytest
 
+HDL: Path = Path(__file__).parent.resolve() / "hdl"
 pytestmark = pytest.mark.skipif(not which("vivado"), reason="Requires Xilinx Vivado")
 
 
 def test_vivado_create_project(tmp_path: Path) -> None:
-    """Test Xilinx Vivado: create_project example <project-dir>."""
+    """Test Xilinx Vivado using pytcl.Vivado helper class: create_project example <project-dir>."""
     with Vivado(cwd=tmp_path) as vivado:
         vivado.create_project("example", tmp_path)
         vivado.close_project()
@@ -21,8 +22,8 @@ def test_vivado_create_project(tmp_path: Path) -> None:
 
 
 def test_vivado_directly_with_pytcl(tmp_path: Path) -> None:
-    """Test Xilinx Vivado: create_project example <project-dir>."""
-    # PyTCL offers some placeholders like `{tcl}` to insert `<pytcl/>executa.tcl`
+    """Test Xilinx Vivado using directly pytcl.PyTCL class: create_project example <project-dir>."""
+    # PyTCL offers some placeholders like `{tcl}` to insert `<pytcl/>execute.tcl`
     cmd: list[str] = [
         "vivado",
         "-nojournal",
@@ -41,6 +42,33 @@ def test_vivado_directly_with_pytcl(tmp_path: Path) -> None:
 
     with PyTCL(*cmd, cwd=tmp_path) as vivado:
         vivado.create_project("example", tmp_path)
+        vivado.close_project()
+
+    assert (tmp_path / "example.xpr").exists()
+
+
+def test_vivado_project_mode(tmp_path: Path) -> None:
+    """Test Xilinx Vivado in project mode."""
+    with Vivado(cwd=tmp_path) as vivado:
+        vivado.create_project("example", tmp_path)
+        vivado.add_files(HDL / "dut.sv")
+
+        synthesis_runs = list(vivado.get_runs("synth_*"))
+        implementation_runs = list(vivado.get_runs("impl_*"))
+
+        vivado.launch_runs(synthesis_runs)
+
+        # wait_on_runs was introduced in Vivado 2021.2. For backward compatibility we will use wait_on_run
+        # https://docs.amd.com/r/2021.2-English/ug835-vivado-tcl-commands/wait_on_runs
+        # Vivado >= 2021.2 can just use: vivado.wait_on_runs(synthesis_runs)
+        for run in synthesis_runs:
+            vivado.wait_on_run(run)
+
+        vivado.launch_runs(implementation_runs)
+
+        for run in implementation_runs:
+            vivado.wait_on_run(run)
+
         vivado.close_project()
 
     assert (tmp_path / "example.xpr").exists()
